8692cdee5fab09c1e183de6f9c4ff2a340ca18be
Harsh Jain <1997harsh.jain@gmail.com>
2022-06-21 21:05:10 +0530

    made path changes

--- dashboard/excel_utils.py
+++ dashboard/excel_utils.py
@@ -13,7 +13,7 @@ def create_excel(work_item):
         title = '-'.join(title.split(' '))
         file_name = f"CR-{id}-{title}.xlsx"
 
-        path = os.path.join(os.getcwd(), 'docs\\CR')
+        path = os.path.join(os.getcwd(), 'docs/CR')
         if not os.path.exists(path):
             os.makedirs(path)
         template = os.path.join(path, "template.xlsx")
@@ -34,8 +34,8 @@ def create_excel(work_item):
 
         workbook.save(cr_excel)
         file_path = os.path.join(path, file_name)
-        return {'message': "CR Document Created",'file_path': file_path, 'file_name': file_name}
+        return {'message': "CR Document Created", 'file_path': file_path, 'file_name': file_name}
 
     except Exception as e:
         print(e)
-        return {'message': "Cannot Create Document",'file_path': None, 'file_name': None}
+        return {'message': "Cannot Create Document", 'file_path': None, 'file_name': None}
